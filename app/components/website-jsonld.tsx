ed9047f911061a4402ea808ac25b65f52c6937dd
iammiori <immioriii@gmail.com>
2025-02-21 22:40:03 +0900

    enhance: website-jsonld

---
+++ app/components/website-jsonld.tsx
@@ -0,0 +1,25 @@
+import { baseUrl } from '@/sitemap'
+
+import { siteConfig } from '@/config/site'
+
+export function WebsiteJsonLd() {
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'WebSite',
+    name: siteConfig.title,
+    description: siteConfig.description,
+    url: baseUrl,
+    potentialAction: {
+      '@type': 'SearchAction',
+      target: `${baseUrl}/search?q={search_term_string}`,
+      'query-input': 'required name=search_term_string',
+    },
+  }
+
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+    />
+  )
+}
