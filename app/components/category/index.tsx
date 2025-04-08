71e56a38eaca78e4860e9dc50d2d7baaa171749e
iammiori <immioriii@gmail.com>
2025-04-08 22:00:06 +0900

    refactor: 안쓰는 파일 삭제

--- app/components/category/index.tsx
+++
@@ -1,8 +0,0 @@
-import { CategoryIcon, CategoryText } from './CategoryItem'
-import CategoryWrapper from './CategoryWrapper'
-
-export const CategoryChip = {
-  Root: CategoryWrapper,
-  Icon: CategoryIcon,
-  Text: CategoryText,
-}
